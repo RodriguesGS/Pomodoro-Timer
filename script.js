05c45bcf2ecee094245c6999fa53ddf38c1f3039
RodriguesGabrielS <rodriguesgasoares@gmail.com>
2023-04-23 20:30:38 -0300

    finish

--- script.js
+++ script.js
@@ -2,69 +2,63 @@ let workTittle = document.querySelector('#work')
 let breakTittle = document.querySelector('#break')
 const audio = new Audio('mp3/alarm.wav')
 
-let workTime = 1
+let workTime = 25
 let breakTime = 5
+
 let seconds = "00"
 
 window.onload = function() {
     document.querySelector('#min').innerHTML = workTime
     document.querySelector('#sec').innerHTML = seconds
-
-    workTittle.classList.add('active')
+    
+    workTittle.classList.add('active');
 }
 
 function start() {
     document.querySelector('#start').style.display = 'none'
     document.querySelector('#restart').style.display = 'flex'
 
-    let workMinutes = workTime - 1
-    let breakMinutes = breakTime - 1
+    let workMinutes = workTime -1
+    let breakMinutes = breakTime -1
+    let breakCount = 0;
 
-    breakCount = 0
-    seconds = 5
+    seconds = 59
 
     function timer() {
         document.querySelector('#min').innerHTML = workMinutes
         document.querySelector('#sec').innerHTML = seconds
         
-        seconds = (seconds -1).toString().padStart(2, "0")
-        
-        if (seconds == 0) {
+        seconds = (seconds - 1).toString().padStart(2, "0")
+
+        if (seconds == -1) {
             workMinutes = (workMinutes - 1).toString().padStart(2, "0")
-            
-            seconds = 5
-        } 
-        if (workMinutes <= -1) {
-            document.querySelector('#min').innerHTML = "00"
-            document.querySelector('#sec').innerHTML = "00"
+            if (workMinutes == -1) {
+                if (breakCount % 2 === 0) {
+                    audio.play()
+                    workMinutes = breakMinutes
+                    breakCount ++
 
+                    workTittle.classList.remove('active');
+                    breakTittle.classList.add('active');
+                } else {
+                    audio.play()
+                    workMinutes = workTime
+                    breakCount++
 
-            audio.play()
-            setInterval(() => {
-                audio.pause()
-                audio.currentTime = 0;
-            }, 1000);
-
-            setInterval(() => {
-                workTittle.classList.remove('active')
-                breakTittle.classList.add('active')
-
-                document.querySelector('#min').innerHTML = breakMinutes
-                document.querySelector('#sec').innerHTML = seconds
-                
-            }, 2000)
-
+                    breakTittle.classList.remove('active');
+                    workTittle.classList.add('active');
+                }
+                seconds = 59
+            } else {
+                seconds = 59
+            }
         }
     }
 
-    setInterval(timer, 1000);
+    const timerInterval = setInterval(timer, 1000);
 }
-
 
 
 function reload() {
     location.reload('index.html')
 }
-
-
-
